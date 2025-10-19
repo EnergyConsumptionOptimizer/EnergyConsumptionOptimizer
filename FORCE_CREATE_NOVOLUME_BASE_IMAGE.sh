3ac3bb9f61a882b909dd31cac3ddcc4c92ab4bd8
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-10-19 21:35:18 +0200

    chore: add scripts to create no volume MongoDB base images

---
+++ FORCE_CREATE_NOVOLUME_BASE_IMAGE.sh
@@ -0,0 +1,41 @@
+
+MONGOOriginalImage="mongo:8.0.15-noble"
+MONGOContainerName="mongodb"
+NoVolumeMONGOImageName="mongonovolume"
+
+# se l'immagine e' gia' nel registry la elimino e la ricreo
+
+FOUND=`docker image ls -q ${NoVolumeMONGOImageName}`
+if [[ -n ${FOUND} ]] ; then 
+	echo "image ${NoVolumeMONGOImageName} esiste gia\', LA ELIMINO E RICREO"; 
+	docker rmi -f ${FOUND}
+fi
+
+echo running container ${MONGOContainerName}
+docker run -itd --name ${MONGOContainerName} ${MONGOOriginalImage}
+
+echo wait until container is up
+NUMATTEMPTS=0
+while OUT=`docker ps --no-trunc --filter "name=^/${MONGOContainerName}" --filter "status=running" --format "{{.ID}}"`; [[ -z ${OUT} ]] ; do ((${NUMATTEMPTS}++)); echo ${NUMATTEMPTS} sleep 1; done
+echo find anonymous volumes
+VOLUMIseparatidavirgole=`docker ps --no-trunc --filter "name=^/${MONGOContainerName}" --filter "status=running" --format "{{.Mounts}}"`
+echo stopping container ${MONGOContainerName}
+docker stop ${MONGOContainerName}
+
+echo export stopped container
+docker export ${MONGOContainerName} > ${MONGOContainerName}_export.tar
+echo import image in local docker registry
+echo docker import ${MONGOContainerName}_export.tar ${NoVolumeMONGOImageName}
+docker import ${MONGOContainerName}_export.tar ${NoVolumeMONGOImageName}
+rm ${MONGOContainerName}_export.tar
+
+echo remove stopped container
+docker rm ${MONGOContainerName}
+echo remove anonymous volumes
+if [[ -n ${VOLUMIseparatidavirgole}  ]] ; then
+  VOLUMIseparatidaspazi=${VOLUMIseparatidavirgole//,/ }
+  docker volume rm -f ${VOLUMIseparatidaspazi}
+fi
+echo lists mongo images
+docker images ${NoVolumeMONGOImage}
+
